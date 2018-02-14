3de3e20d4d2b798034a13f3f5a32939da7fde08c
Zach Brown <brownzach125@gmail.com>
2018-02-13 21:22:30 -0600

    Working Implementation of DefaultBWListener and I can remote debug!
    Signed-off-by: Zach Brown <brownzach125@gmail.com>

--- AI/Lib/bot.py
+++ AI/Lib/bot.py
@@ -1,4 +1,21 @@
+from bwapi import DefaultBWListener, Mirror
 
-
-def test_bot():
-    print "Im a bot"
+class Bot(DefaultBWListener):
+    def __init__(self):
+        self.mirror = Mirror()
+        self.game = None
+        self.self = None
+        
+    def run(self):
+        self.mirror.getModule().setEventListener(self)
+        self.mirror.startGame()
+        
+    def onUnitCreate(self, unit):
+        print "New Unit Discovered"
+        
+    def onStart(self):
+        self.game = self.mirror.getGame()
+        print "On Start"
+        
+    def onFrame(self):
+        print "On Frame"
--- AI/__run__.py
+++ AI/__run__.py
@@ -1,21 +1,12 @@
 import sys
 import os
 
-# Add Lib files to jython python path
-bot_folder = os.path.join(os.getenv('BOT_DIR'), os.getenv('BOT_NAME'), "AI")
-bot_lib_folder = os.path.join(bot_folder, 'Lib')
-sys.path.append(bot_lib_folder)
-
-
-#sys.path.append('z:/app/pysrc')
-sys.path.append(os.path.join(bot_folder, 'pysrc'))
+pysrc_folder = os.getenv('BOT_DIR') + "/" + os.getenv('BOT_NAME') + "/AI/pysrc"
+sys.path.append(pysrc_folder)
 import pydevd
-pydevd.settrace('172.17.0.1')
+pydevd.settrace('172.17.0.1', stdoutToServer=True, stderrToServer=True)
 
-#print sys.path
-from bot import test_bot
-from bwapi import DefaultBWListener
-from bwapi import Mirror
+from Lib.bot import Bot
 
-test_bot()
-print "Hello World"
+Bot().run()
+
